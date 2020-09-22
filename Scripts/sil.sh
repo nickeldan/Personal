803b43167e14ed9c46969816ba14bdaba0fc8beb
nickeldan <nickeldan@gmail.com>
2020-09-22 13:23:31 -0400

    moved *rc into a ConfigFiles directory; added Scripts/sil.sh

---
+++ Scripts/sil.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+usage() {
+	echo "Usage: sil <executable> [<arguments>]"
+}
+
+if [ $# -eq 0 ]; then
+	usage
+	exit 1
+fi
+
+if [ "$1" = "-h" ]; then
+	usage
+	exit 0
+fi
+
+if [[ ! -x $(which $1) ]]; then
+	echo "'$1' is not executable."
+	exit 2
+fi
+
+$@ 2> /dev/null &
+echo $!
