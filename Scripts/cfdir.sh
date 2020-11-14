ec71adfea279dc4e32e68f17a5d108f414960139
nickeldan <nickeldan@gmail.com>
2020-11-13 23:40:34 -0500

    added a shopt line to cfdir.sh

--- Scripts/cfdir.sh
+++ Scripts/cfdir.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+shopt -s nullglob
+
 if [ $# -eq 0 ]; then
     echo "usage: $0 <directory>"
     exit 1
@@ -8,10 +10,6 @@ fi
 dir=$1
 if [ ! -d $dir ]; then
     echo "$dir is not a directory."
-    exit 2
-fi
-if [ ! -w $dir ]; then
-    echo "No write permission for $dir."
     exit 2
 fi
 
