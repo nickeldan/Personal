280c3b6e8ab5dd9afcb8992390fdf2605bb1db44
nickeldan <nickeldan@gmail.com>
2021-05-26 18:51:08 -0400

    added flake8 and check_py_file.sh; added an alias for black; added the bash function sudoln; added vim commands for black and clang-format

---
+++ Scripts/check_py_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+LINE_LENGTH=110
+
+black_cmd() {
+    black -l $LINE_LENGTH $@
+}
+
+flake8_cmd() {
+    flake8 --max-line-length $LINE_LENGTH $@
+}
+
+if [ $# -eq 0 ]; then
+    files=$(find . -path '*/\.*' -prune -false -o -name '*.py')
+    black_cmd $files
+    flake8_cmd $files
+else
+    filename=$1
+    black_cmd $filename
+    flake8_cmd $filename
+fi
